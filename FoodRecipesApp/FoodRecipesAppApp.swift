7f110d8562ab48a997da487d0e219890bc868c33
Andres Cusirramos <andres.cusirramos@ucsp.edu.pe>
2022-06-11 16:19:50 -0500

    first commit

---
+++ FoodRecipesApp/FoodRecipesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodRecipesAppApp.swift
+//  FoodRecipesApp
+//
+//  Created by Andres  on 11/06/22.
+//
+
+import SwiftUI
+
+@main
+struct FoodRecipesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
